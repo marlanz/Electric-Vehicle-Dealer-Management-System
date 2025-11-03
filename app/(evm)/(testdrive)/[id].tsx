1033053ba239c0fee94994a4729334c6cbc3bad5
marlanz <phucanhdodang1211@gmail.com>
2025-11-03 21:02:22 +0700

    feat: add ui for create test drive appointment page

---
+++ app/(evm)/(testdrive)/[id].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const AppointmentDetail = () => {
+  return (
+    <View>
+      <Text>AppointmentDetail</Text>
+    </View>
+  );
+};
+
+export default AppointmentDetail;
